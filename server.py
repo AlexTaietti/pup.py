f11419609740d41d998cc3b169c92acb7a83bbe0
Alex Taietti <taiettialex@yahoo.it>
2023-01-14 13:13:53 +0000

    server: add CORS configuration

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from puppy import pup
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/fetch', methods=['POST'])
 def fetch():
     data = request.json
-    return jsonify(data)
+    start = data["start"]
+    target = data["target"]
+    pupper = pup.Puppy(start, target)
+    result = pupper.run()
+    return result
 
 app.run()
